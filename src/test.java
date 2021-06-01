a741b4d0e3783a279447a79dbd952e0b73ac625c
yuzhi535 <zhouyuxi.abc@gmail.com>
2021-06-01 19:22:34 +0800

    update

--- src/test.java
+++ src/test.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class test {
+import java.io.IOException;
+import java.net.Socket;
+
+public class test {
+    test(String url, int port) {
+        try {
+            Socket s = new Socket(url, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new test("localhost", 7777);
+    }
 }
