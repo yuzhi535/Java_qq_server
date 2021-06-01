6ee192b39402d2108ea73aeb3b4eb1782401d8f6
yuzhi535 <zhouyuxi.abc@gmail.com>
2021-06-01 12:02:03 +0800

    update

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,16 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     Server(int ip) {
         try {
-            ServerSocket serverSocket = new ServerSocket();
+            ServerSocket serverSocket = new ServerSocket(7777);
+            try (Socket s = serverSocket.accept()) {
+
+            } catch (IOException e) {
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
